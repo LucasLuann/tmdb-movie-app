f0b2eb00f845c277430a38d310ace826233d2a21
Lucas Luann <75109074+LucasLuann@users.noreply.github.com>
2025-02-24 13:34:42 -0400

    Feat: Adicionando a linguagem PT-BR

--- src/lib/tmdb.ts
+++ src/lib/tmdb.ts
@@ -3,7 +3,7 @@ const TMDB_BASE_URL = 'https://api.themoviedb.org/3'
 
 export async function fetchMovies(query: string) {
   const response = await fetch(
-    `${TMDB_BASE_URL}/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(query)}`
+    `${TMDB_BASE_URL}/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(query)}&language=pt-BR`
   )
   console.log('fecthMovies ',response)
   if(!response.ok){
@@ -15,7 +15,7 @@ export async function fetchMovies(query: string) {
 
 export async function fetchPopularMovies(){
   console.log(TMDB_API_KEY)
-  const response = await fetch(`${TMDB_BASE_URL}/movie/popular?api_key=${TMDB_API_KEY}`)
+  const response = await fetch(`${TMDB_BASE_URL}/movie/popular?api_key=${TMDB_API_KEY}&language=pt-BR`)
 
   if(!response.ok){
     throw new Error('Falha ao buscar filmes populares')
